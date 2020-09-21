fec11453d9c069d39c85be9eb380d284126327c8
Dean Wilson <dean.wilson@gmail.com>
2020-09-21 08:47:17 +0100

    Initial code drop - print the environ variables but replace sensitive values
    Start by redacting the more common values - but hard coded

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func ToRedact() []string {
+
+	var redactedNames = []string{
+		"AWS_ACCESS_KEY_ID",
+		"AWS_SECRET_ACCESS_KEY",
+		"GITHUB_TOKEN",
+		"GITHUB_AUTH_TOKEN"}
+
+	return redactedNames
+}
+
+func main() {
+	redactedVariables := ToRedact()
+
+	for _, envvar := range os.Environ() {
+		pair := strings.SplitN(envvar, "=", 2)
+
+		name, value := pair[0], pair[1]
+
+		for _, redacted := range redactedVariables {
+			if name == redacted {
+				value = "XXXXXXXX"
+			}
+		}
+
+		fmt.Println(name + "=" + value)
+	}
+}
