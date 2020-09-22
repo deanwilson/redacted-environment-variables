e0b23333dbc53b8a192a2f16e6614c688ab4726f
Dean Wilson <dean.wilson@gmail.com>
2020-09-22 18:15:44 +0100

    Use go fmt to format the code

--- main.go
+++ main.go
@@ -2,67 +2,67 @@ package main
 
 import (
 	"fmt"
-  "path/filepath"
+	"github.com/pelletier/go-toml"
 	"os"
+	"path/filepath"
 	"strings"
-  "github.com/pelletier/go-toml"
 )
 
 func DefaultConfig() *toml.Tree {
 
-  config, _ := toml.Load(`
+	config, _ := toml.Load(`
   [config]
   redacted = "123123123"
   `)
 
-  return config
+	return config
 
 }
 
 func LocateConfigFile(filename string) string {
-  var paths []string // places to search
-  var config_path string  // the located config file
+	var paths []string     // places to search
+	var config_path string // the located config file
 
-  current, err := os.Getwd()
-  if err != nil {
-    fmt.Println("Failed to get current working directory: ", err)
-    os.Exit(1)
-  }
+	current, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Failed to get current working directory: ", err)
+		os.Exit(1)
+	}
 
-  // local directory first
-  paths = append(paths, filepath.Join(current, filename))
+	// local directory first
+	paths = append(paths, filepath.Join(current, filename))
 
-  // then the home directory
-  homedir, err := os.UserHomeDir()
-  paths = append(paths, filepath.Join(homedir, filename))
+	// then the home directory
+	homedir, err := os.UserHomeDir()
+	paths = append(paths, filepath.Join(homedir, filename))
 
-  // then fail back to system wide /etc
-  paths = append(paths, filepath.Join("/etc", filename))
+	// then fail back to system wide /etc
+	paths = append(paths, filepath.Join("/etc", filename))
 
-  for _, path := range paths {
-    _, err = os.Stat(path)
-    if !os.IsNotExist(err) {
-      config_path = path
-      break
-    }
-  }
+	for _, path := range paths {
+		_, err = os.Stat(path)
+		if !os.IsNotExist(err) {
+			config_path = path
+			break
+		}
+	}
 
-  return config_path
+	return config_path
 }
 
 func LoadConfig(filename string) *toml.Tree {
-  var config *toml.Tree
+	var config *toml.Tree
 
-  config_file := LocateConfigFile(filename)
+	config_file := LocateConfigFile(filename)
 
-  // If a config file is found use, otherwise defaults
-  if config_file != "" {
-    config, _ = toml.LoadFile(config_file)
-  } else {
-    config = DefaultConfig()
-  }
+	// If a config file is found use, otherwise defaults
+	if config_file != "" {
+		config, _ = toml.LoadFile(config_file)
+	} else {
+		config = DefaultConfig()
+	}
 
-  return config
+	return config
 }
 
 func ToRedact() []string {
@@ -78,8 +78,8 @@ func ToRedact() []string {
 }
 
 func main() {
-  config := LoadConfig(".redacted-environment.toml")
-  redacted_string := config.Get("config.redacted").(string)
+	config := LoadConfig(".redacted-environment.toml")
+	redacted_string := config.Get("config.redacted").(string)
 
 	redactedVariables := ToRedact()
 
